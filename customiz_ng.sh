89564851df9ccd5ec685d11c5206a00fd180c936
Vladimir Paltsev <vlap-d@yandex.ru>
2025-12-06 17:01:24 +0500

    Add files via upload

--- customiz_ng.sh
+++ customiz_ng.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+!/bin/bash
 
-git clone https://github.com/vlapvg/backup_ng.git; cd /home/vlap/backup_ng.git; cp default /etc/nginx/sites-enabled; cp filebeat.yml /etc/filebeat; systemctl start nginx; systemctl enable nginx; system start filebeat; systemctl enable filebeat
+git clone https://github.com/vlapvg/backup_ng.git; cd /home/vlap/backup_ng.git; cp default /etc/nginx/sites-enabled; cp filebeat.yml /etc/filebeat; systemctl start nginx; system start filebeat
+
+
