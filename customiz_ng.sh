df8364f62ff8ff1f8d4aa70943b25b9358c97c56
Vladimir Paltsev <vlap-d@yandex.ru>
2025-12-06 17:06:33 +0500

    Update customiz_ng.sh

--- customiz_ng.sh
+++ customiz_ng.sh
@@ -1,5 +1,5 @@
 !/bin/bash
 
-git clone https://github.com/vlapvg/backup_ng.git; cd /home/vlap/backup_ng.git; cp default /etc/nginx/sites-enabled; cp filebeat.yml /etc/filebeat; systemctl start nginx; system start filebeat
+cd /home/vlap/backup_ng.git; cp default /etc/nginx/sites-enabled; cp filebeat.yml /etc/filebeat; systemctl start nginx; system start filebeat
 
 
